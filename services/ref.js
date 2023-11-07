f87f214a8a38c1c3284daf356e780dcdd5ec8bd5
Olak-git <olakami.ulerich.bonou@gmail.com>
2023-11-07 08:45:13 +0100

    Modification de reference vers l'api local

--- services/ref.js
+++ services/ref.js
@@ -1,5 +1,5 @@
 import { HOST, PRODUCTION } from "../constants/data"
 
-const ref = PRODUCTION ? "https://ebenely.project.utechaway.com/api_ebenely/" : `${HOST}/projects/ebenely/api_ebenely/`;
+const ref = PRODUCTION ? "https://ebenely.project.utechaway.com/api_ebenely/" : `${HOST}/projects/ebenely-htdocs/api_ebenely/`;
 
 export default ref
--- services/ref_public.js
+++ services/ref_public.js
@@ -1,5 +1,5 @@
 import { HOST, PRODUCTION } from "../constants/data"
 
-const ref_public = PRODUCTION ? "https://ebenely.project.utechaway.com/" : `${HOST}/projects/ebenely` // http://www.wylbee.company/superwoman/
+const ref_public = PRODUCTION ? "https://ebenely.project.utechaway.com/" : `${HOST}/projects/ebenely-htdocs` // http://www.wylbee.company/superwoman/
 
 export default ref_public
